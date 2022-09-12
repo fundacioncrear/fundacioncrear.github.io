35d4c715f5644dfd3f7e3230b168e9cf045bb1fc
eorma2000 <110936444+eorma2000@users.noreply.github.com>
2022-09-12 11:04:03 -0500

    Update multiplicacion.js

--- js/juegos/matematicas/operaciones basicas/multiplicacion.js
+++ js/juegos/matematicas/operaciones basicas/multiplicacion.js
@@ -39,6 +39,7 @@ option1.addEventListener("click", function(){
   if(option1.innerHTML == answer){
     generate_equation();
     winAudio.play();
+    againAudio.play();
     swal({
       position: 'top-end',
       icon: 'success',
@@ -58,6 +59,7 @@ option2.addEventListener("click", function(){
   if(option2.innerHTML == answer){
     generate_equation();
     winAudio.play();
+    againAudio.play();
     swal({
       position: 'top-end',
       icon: 'success',
@@ -77,6 +79,7 @@ option3.addEventListener("click", function(){
   if(option3.innerHTML == answer){
     generate_equation();
     winAudio.play();
+    againAudio.play();
     swal({
       position: 'top-end',
       icon: 'success',
